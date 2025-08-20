971cb5a905ec7fd9f2f52ce5a0c7c712c490c29b
wchanchavac <william.chanchavac@gmail.com>
2025-08-19 18:40:54 -0600

    Refactor Vercel configuration and GraphQL resolvers for improved clarity
    - Updated the `includeFiles` property in the Vercel configuration to specifically include GraphQL type files, enhancing deployment accuracy.
    - Refactored the GraphQL resolvers to directly import and list all resolver modules, simplifying the loading process and improving maintainability.
    - Commented out unused imports and console logs to clean up the codebase.

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { loadFiles } from '@graphql-tools/load-files';
+// import { loadFiles } from '@graphql-tools/load-files';
 import { mergeResolvers } from '@graphql-tools/merge';
 import {
   BigIntResolver,
@@ -13,18 +13,56 @@ import {
   URLResolver,
   UUIDResolver,
 } from 'graphql-scalars';
-import path from 'path';
-import { getDirname } from '../utils/path.js';
+// import path from 'path';
 
-const __dirname = getDirname(import.meta.url);
+import areaResolver from './area.resolver.js';
+import countryResolver from './country.resolver.js';
+import departmentResolver from './department.resolver.js';
+import globalResolver from './global.resolver.js';
+import jobResolver from './job.resolver.js';
+import levelResolver from './level.resolver.js';
+import measurementUnitResolver from './measurementUnit.resolver.js';
+import organizationResolver from './organization.resolver.js';
+import regionResolver from './region.resolver.js';
+import serverResolver from './server.resolver.js';
+import serviceResolver from './service.resolver.js';
+import serviceTypeResolver from './serviceType.resolver.js';
+import siteResolver from './site.resolver.js';
+import specialityResolver from './speciality.resolver.js';
+import stateResolver from './state.resolver.js';
+import statusResolver from './status.resolver.js';
+import subzoneResolver from './subzone.resolver.js';
+import technologyResolver from './technology.resolver.js';
+import userResolver from './user.resolver.js';
+import vendorResolver from './vendor.resolver.js';
+import zoneResolver from './zone.resolver.js';
 
-const resolversArray = await loadFiles(path.join(__dirname, './'), {
-  extensions: ['.js'],
-  ignoreIndex: true,
-});
+const resolversArray = [
+  areaResolver,
+  countryResolver,
+  departmentResolver,
+  globalResolver,
+  jobResolver,
+  levelResolver,
+  measurementUnitResolver,
+  organizationResolver,
+  regionResolver,
+  serverResolver,
+  serviceResolver,
+  serviceTypeResolver,
+  siteResolver,
+  specialityResolver,
+  stateResolver,
+  statusResolver,
+  subzoneResolver,
+  technologyResolver,
+  userResolver,
+  vendorResolver,
+  zoneResolver,
+];
 
 console.log('the amount of resolvers', resolversArray.length);
-console.log('the path', path.join(__dirname, './'));
+// console.log('the path', path.join(__dirname, './'));
 
 export default mergeResolvers([
   ...resolversArray,
