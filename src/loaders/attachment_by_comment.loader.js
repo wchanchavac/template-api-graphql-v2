d2b2dbd351a61ca7a0bed2380067d2af1bd54e77
wchanchavac <william.chanchavac@gmail.com>
2025-08-29 12:16:35 -0600

    Refactor attachmentByCommentLoader for improved readability and caching options
    - Reformatted the attachmentByCommentLoader function for better readability by adjusting indentation and structure.
    - Added a cache option set to false to the DataLoader configuration, allowing for more flexible caching behavior.
    - Ensured that the filtering logic for attachments remains intact while enhancing code clarity.

--- src/loaders/attachment_by_comment.loader.js
+++ src/loaders/attachment_by_comment.loader.js
@@ -1,22 +1,28 @@
 import DataLoader from 'dataloader';
 import Attachment from '#models/attachment.model';
 
-const attachmentByCommentLoader = new DataLoader(async (commentIds) => {
-  const attachments = await Attachment.findAll({
-    attributes: {
-      exclude: ['updatedAt', 'deletedAt', 'data'],
-    },
-    where: {
-      commentId: commentIds,
-    },
-    order: [['createdAt', 'ASC']],
-  });
+const attachmentByCommentLoader = new DataLoader(
+  async (commentIds) => {
+    const attachments = await Attachment.findAll({
+      attributes: {
+        exclude: ['updatedAt', 'deletedAt', 'data'],
+      },
+      where: {
+        commentId: commentIds,
+      },
+      order: [['createdAt', 'ASC']],
+    });
 
-  return commentIds.map(
-    (commentId) =>
-      attachments.filter((attachment) => attachment.commentId === commentId) ||
-      [],
-  );
-});
+    return commentIds.map(
+      (commentId) =>
+        attachments.filter(
+          (attachment) => attachment.commentId === commentId,
+        ) || [],
+    );
+  },
+  {
+    cache: false,
+  },
+);
 
 export default attachmentByCommentLoader;
